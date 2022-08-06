81df897be33b65995aec8a67be3081f228b95728
Hope Olaide <hopeolaide@gmail.com>
2022-08-06 14:55:32 -0400

    modified: factaarray nested directory **commented out** for now

--- pages/factarray/[factId].js
+++ pages/factarray/[factId].js
@@ -1,18 +1,18 @@
-import { useRouter } from "next/router"
+// import { useRouter } from "next/router"
 
-export default function FactDetails() {
-  const router = useRouter()
-  const factId = router.query.factId
-  return (
-    <div>
-      <h2>Details about this specific historical fact: </h2>
-      <h1>Fact ID: {factId}</h1>
+// export default function FactDetails() {
+//   const router = useRouter()
+//   const factId = router.query.factId
+//   return (
+//     <div>
+//       <h2>Details about this specific historical fact: </h2>
+//       <h1>Fact ID: {factId}</h1>
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
 
 
-//Next js treats [square bracketed] names as a dynamic segment in a dynamic route.
-//Next js will priorities specific routes over dynamic routes with the same id/name/value at the end
+// //Next js treats [square bracketed] names as a dynamic segment in a dynamic route.
+// //Next js will priorities specific routes over dynamic routes with the same id/name/value at the end
--- pages/factarray/index.js
+++ pages/factarray/index.js
@@ -1,38 +1,38 @@
 
-export default function FactList({ factarray }) {
-  // const factarray = 
-  return (
-    <div>
-      <h1>
-        Location-based fact list
-      </h1>
-      <h2>Historical Fact List</h2>
-      <p>This is a list of json objects</p>
-      <ul>
-        {factarray.map(fact => {
-          return (
-            <li key={fact._id}>
-              {fact.title}
-            </li>
-          )
-        })}
-        <li>Fact #1</li>
-        <li>Fact #2</li>
-        <li>Fact #3</li>
-        <li>Fact #4</li>
-        <li>Fact #5</li>
-      </ul>
+// export default function FactList({ factarray }) {
+//   // const factarray = 
+//   return (
+//     <div>
+//       <h1>
+//         Location-based fact list
+//       </h1>
+//       <h2>Historical Fact List</h2>
+//       <p>This is a list of json objects</p>
+//       <ul>
+//         {factarray.map(fact => {
+//           return (
+//             <li key={fact._id}>
+//               {fact.title}
+//             </li>
+//           )
+//         })}
+//         <li>Fact #1</li>
+//         <li>Fact #2</li>
+//         <li>Fact #3</li>
+//         <li>Fact #4</li>
+//         <li>Fact #5</li>
+//       </ul>
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export async function getStaticProps() {
-  const factarray = await fetch('https://api.si.edu/openaccess/api/v1.0/terms/:category').then(r => r.json());
-  // console.log(factlist)
-  return {
-    props: {
-      factarray
-    }
-  }
-}
+// export async function getStaticProps() {
+//   const factarray = await fetch('https://api.si.edu/openaccess/api/v1.0/terms/:category').then(r => r.json());
+//   // console.log(factlist)
+//   return {
+//     props: {
+//       factarray
+//     }
+//   }
+// }
